67bcfebe22403693a8e84940c706ce2bd51685b0
ATerenteva <ATerenteva@inno.tech>
2023-01-13 21:45:40 +0300

    added a row counter

--- src/ExceededTheLimit.java
+++ src/ExceededTheLimit.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ExceededTheLimit {
+public class ExceededTheLimit extends RuntimeException{
+
+    public ExceededTheLimit() {
+        super();
+    }
+
+    public ExceededTheLimit(String message) {
+        super(message);
+    }
+
+    public ExceededTheLimit(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ExceededTheLimit(Throwable cause) {
+        super(cause);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int count = 0;
+        int stringSum = 0;
+        int lenghtMax = 0;
+        int lenghtMin = 1024;
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
@@ -20,6 +23,25 @@ public class Main {
             count++;
             System.out.println("Путь указан верно.");
             System.out.println("Это файл номер " + count);
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader =
+                        new BufferedReader(fileReader);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    if(length>1024) throw new ExceededTheLimit("The number of characters in the string should not be more than 1024");
+                    if(length>lenghtMax) lenghtMax = length;
+                    if(length<lenghtMin) lenghtMin = length;
+                    stringSum+=1;
+                }
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+            System.out.println("Количество строк: " + stringSum);
+            System.out.println("Длина самой длинной строки: " + lenghtMax);
+            System.out.println("Длина самой короткой строки: " + lenghtMin);
         }
     }
 }
