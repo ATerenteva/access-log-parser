e429a5ac720d2b8892672e59c3eaeaa01a15f162
ATerenteva <ATerenteva@inno.tech>
2023-02-01 21:14:26 +0300

    added a list of available pages and an operating system share counter

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,10 +1,14 @@
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Statistics {
     long totalTraffic;
     LocalDateTime minTime;
     LocalDateTime maxTime;
+    private static HashSet<String> pages = new HashSet<>();
+    private static HashMap<String, Integer> osStatistic = new HashMap<>();
 
     public Statistics() {
         this.totalTraffic = 0;
@@ -16,8 +20,29 @@ public class Statistics {
         if (logEntry.getTime().isBefore(this.minTime)) this.minTime = logEntry.getTime();
         if (logEntry.getTime().isAfter(this.maxTime)) this.maxTime = logEntry.getTime();
         this.totalTraffic += logEntry.getResponseSize();
+        if (logEntry.getResponseCode() == 200) pages.add(logEntry.getPath());
+        if (!(logEntry.getUserAgent().getOs() == null)){
+            if (osStatistic.containsKey(logEntry.getUserAgent().getOs())) osStatistic.put(logEntry.getUserAgent().getOs(), osStatistic.get(logEntry.getUserAgent().getOs())+1);
+            else osStatistic.put(logEntry.getUserAgent().getOs(), 1);
+        }
     }
     public long getTrafficRate (){
         return (long)(this.totalTraffic / ((double)ChronoUnit.MINUTES.between(this.minTime, this.maxTime)/60));
+    }
+
+    public HashSet<String> getExistingPages() {
+        return pages;
+    }
+
+    public HashMap<String, Double> osStatistics(){
+        double count = 0;
+        HashMap<String, Double> h = new HashMap<>();
+        for (Integer s : osStatistic.values()){
+            count += s;
+        }
+        for (String s : osStatistic.keySet()){
+            h.put(s, (double)osStatistic.get(s)/count);
+        }
+        return h;
     }
 }
